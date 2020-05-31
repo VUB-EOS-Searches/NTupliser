eca166acd4a68a7f307b35002a8031b9b98a33be
davidcarbonis <alexander.david.morton@cern.ch>
2020-05-31 18:51:26 +0100

    bash scripts to aid MC skimmingn

---
+++ NTupliser/test/skim_HZJ.sh
@@ -0,0 +1,83 @@
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS55_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS55_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS55_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS55_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS55_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS55_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS55_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS55_ctauS1000/
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS40_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS40_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS40_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS40_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS40_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS40_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS40_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS40_ctauS1000/
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS2_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS2_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS2_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS2_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS2_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS2_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS2_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS2_ctauS1000/
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS1_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS1_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS1_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS1_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS1_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS1_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS1_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS1_ctauS1000/
+
+###
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS55_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS55_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS55_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS55_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS55_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS55_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS55_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS55_ctauS1000/
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS40_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS40_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS40_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS40_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS40_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS40_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS40_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS40_ctauS1000/
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS2_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS2_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS2_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS2_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS2_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS2_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS2_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS2_ctauS1000/
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS1_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS1_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS1_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS1_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS1_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS1_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS1_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS1_ctauS1000/
+
+###
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS55_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS55_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS55_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS55_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS55_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS55_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS55_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS55_ctauS1000/
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS40_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS40_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS40_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS40_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS40_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS40_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS40_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS40_ctauS1000/
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS2_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS2_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS2_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS2_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS2_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS2_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS2_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS2_ctauS1000/
+
+###
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS55_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS55_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS55_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS55_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS55_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS55_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS55_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS55_ctauS1000/
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS40_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS40_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS40_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS40_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS40_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS40_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS40_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS40_ctauS1000/
+
+### 
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS55_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS55_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS55_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS55_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS55_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS55_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS55_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS55_ctauS1000/
+
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS40_ctauS1/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS40_ctauS1/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS40_ctauS10/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS40_ctauS10/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS40_ctauS100/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS40_ctauS100/
+skim --MC --LHE -i /vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS40_ctauS1000/ -o /vols/cms/adm10/skims2017/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS40_ctauS1000/
+
