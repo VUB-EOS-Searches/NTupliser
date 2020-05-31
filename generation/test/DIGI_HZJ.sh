c38b51e202a608da10a76c4375a34f9a3735de06
Alexander Morton <alexander.david.morton.cern.ch>
2020-05-31 20:32:15 +0200

    scp aid files for transfering between IC and lxplus for step 1

--- generation/test/DIGI_HZJ.sh
+++ generation/test/DIGI_HZJ.sh
@@ -20,25 +20,25 @@ cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/
 
 ###
 
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS55_ctauS1.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS55_ctauS1.root
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS55_ctauS10.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS55_ctauS10.root
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS55_ctauS100.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS55_ctauS100.root
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS55_ctauS1000.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS55_ctauS1000.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS55_ctauS1.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS55_ctauS1.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS55_ctauS10.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS55_ctauS10.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS55_ctauS100.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS55_ctauS100.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS55_ctauS1000.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS55_ctauS1000.root
 
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS40_ctauS1.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS40_ctauS1.root
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS40_ctauS10.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS40_ctauS10.root
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS40_ctauS100.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS40_ctauS100.root
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS40_ctauS1000.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS40_ctauS1000.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS40_ctauS1.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS40_ctauS1.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS40_ctauS10.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS40_ctauS10.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS40_ctauS100.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS40_ctauS100.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS40_ctauS1000.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS40_ctauS1000.root
 
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS2_ctauS1.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS2_ctauS1.root
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS2_ctauS10.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS2_ctauS10.root
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS2_ctauS100.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS2_ctauS100.root
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS2_ctauS1000.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS2_ctauS1000.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS2_ctauS1.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS2_ctauS1.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS2_ctauS10.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS2_ctauS10.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS2_ctauS100.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS2_ctauS100.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS2_ctauS1000.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS2_ctauS1000.root
 
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS1_ctauS1.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS1_ctauS1.root
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS1_ctauS10.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS1_ctauS10.root
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS1_ctauS100.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS1_ctauS100.root
-cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_dddd_MS1_ctauS1000.root outputFile=file:/tmp/almorton/step1_HZJ_dddd_MS1_ctauS1000.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS1_ctauS1.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS1_ctauS1.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS1_ctauS10.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS1_ctauS10.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS1_ctauS100.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS1_ctauS100.root
+cmsRun step1_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT_2017.py inputFiles=file:/tmp/almorton/GEN-SIM_HZJ_uuuu_MS1_ctauS1000.root outputFile=file:/tmp/almorton/step1_HZJ_uuuu_MS1_ctauS1000.root
 
 ###
 
