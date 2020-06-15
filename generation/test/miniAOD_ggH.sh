b5d8600a1e14d7ee46dfb56f3d00aae856b0022e
davidcarbonis <alexander.david.morton@cern.ch>
2020-06-15 17:15:42 +0100

    Initial commit

---
+++ generation/test/miniAOD_ggH.sh
@@ -0,0 +1,82 @@
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS55_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS55_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS55_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_10K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS55_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_10K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS55_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS55_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS55_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS55_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS40_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS40_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS40_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_10K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS40_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_10K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS40_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS40_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS40_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS40_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS2_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS2_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS2_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_10K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS2_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_10K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS2_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS2_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS2_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS2_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS1_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS1_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS1_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_10K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS1_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_10K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS1_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS1_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS1_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTodddd_MH125_MS1_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+####
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS55_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS55_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS55_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS55_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS55_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS55_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS55_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS55_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS40_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS40_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS40_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS40_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS40_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS40_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS40_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS40_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS2_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS2_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS2_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS2_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS2_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS2_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS2_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS2_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS1_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS1_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS1_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS1_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS1_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS1_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS1_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTouuuu_MH125_MS1_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root	
+
+####
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS55_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS55_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS55_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS55_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS55_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS55_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS55_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS55_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS40_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS40_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS40_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS40_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS40_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS40_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS40_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS40_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS2_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS2_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS2_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS2_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS2_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS2_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS2_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTossss_MH125_MS2_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+####
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS55_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS55_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS55_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS55_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS55_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS55_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS55_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS55_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS40_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS40_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS40_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS40_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS40_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS40_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS40_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTocccc_MH125_MS40_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+####
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS55_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS55_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS55_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS55_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS55_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS55_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS55_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS55_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS40_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS40_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS40_ctauS10_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS40_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS40_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS40_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS40_ctauS1000_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_HToSSTobbbb_MH125_MS40_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
