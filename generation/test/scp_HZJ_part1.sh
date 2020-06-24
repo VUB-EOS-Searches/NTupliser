e06011a1f352716e57a07f2cb39b814331f702f2
Alexander Morton <alexander.david.morton.cern.ch>
2020-06-25 01:44:45 +0200

    Update scp files

--- generation/test/scp_HZJ_part1.sh
+++ generation/test/scp_HZJ_part1.sh
@@ -1,84 +1,19 @@
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS55_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS55_ctauS1.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS55_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS55_ctauS10.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS55_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS55_ctauS100.root
-######scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS55_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS55_ctauS1000.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS55_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS55_ctauS1.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS55_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS55_ctauS10.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS55_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS55_ctauS100.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS55_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS55_ctauS1000.root
 
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS40_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS40_ctauS1.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS40_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS40_ctauS10.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS40_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS40_ctauS100.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS40_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS40_ctauS1000.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS40_ctauS1.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS40_ctauS10.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS40_ctauS100.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS40_ctauS1000.root
 
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS2_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS2_ctauS1.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS2_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS2_ctauS10.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS2_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS2_ctauS100.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS2_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS2_ctauS1000.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS2_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS2_ctauS1.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS2_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS2_ctauS10.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS2_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS2_ctauS100.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS2_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS2_ctauS1000.root
 
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS1_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS1_ctauS1.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS1_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS1_ctauS10.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS1_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS1_ctauS100.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTodddd_Vleptonic_M125_MS1_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_dddd_MS1_ctauS1000.root
-
-###
-
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS55_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS55_ctauS1.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS55_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS55_ctauS10.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS55_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS55_ctauS100.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS55_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS55_ctauS1000.root
-
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS40_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS40_ctauS1.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS40_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS40_ctauS10.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS40_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS40_ctauS100.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS40_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS40_ctauS1000.root
-
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS2_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS2_ctauS1.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS2_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS2_ctauS10.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS2_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS2_ctauS100.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS2_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS2_ctauS1000.root
-
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS1_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS1_ctauS1.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS1_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS1_ctauS10.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS1_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS1_ctauS100.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTouuuu_Vleptonic_M125_MS1_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_uuuu_MS1_ctauS1000.root
-
-###
-
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS55_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_ssss_MS55_ctauS1.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS55_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_ssss_MS55_ctauS10.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS55_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_ssss_MS55_ctauS100.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS55_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_ssss_MS55_ctauS1000.root
-
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS40_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_ssss_MS40_ctauS1.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS40_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_ssss_MS40_ctauS10.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS40_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_ssss_MS40_ctauS100.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS40_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_ssss_MS40_ctauS1000.root
-
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS2_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_ssss_MS2_ctauS1.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS2_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_ssss_MS2_ctauS10.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS2_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_ssss_MS2_ctauS100.root
-scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTossss_Vleptonic_M125_MS2_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_ssss_MS2_ctauS1000.root
-
-
-###
-
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS55_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_cccc_MS55_ctauS1.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS55_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_cccc_MS55_ctauS10.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS55_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_cccc_MS55_ctauS100.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS55_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_cccc_MS55_ctauS1000.root
-
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS40_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_cccc_MS40_ctauS1.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS40_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_cccc_MS40_ctauS10.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS40_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_cccc_MS40_ctauS100.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTocccc_Vleptonic_M125_MS40_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_cccc_MS40_ctauS1000.root
-
-###
-
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS55_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_bbbb_MS55_ctauS1.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS55_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_bbbb_MS55_ctauS10.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS55_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_bbbb_MS55_ctauS100.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS55_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_bbbb_MS55_ctauS1000.root
-
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS40_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_bbbb_MS40_ctauS1.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS40_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_bbbb_MS40_ctauS10.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS40_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_bbbb_MS40_ctauS100.root
-###### scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSSTobbbb_Vleptonic_M125_MS40_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_bbbb_MS40_ctauS1000.root
-
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS1_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS1_ctauS1.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS1_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS1_ctauS10.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS1_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS1_ctauS100.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS1_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_HZJ_MS1_ctauS1000.root
