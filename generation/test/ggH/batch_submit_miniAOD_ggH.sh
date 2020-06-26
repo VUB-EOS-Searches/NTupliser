100611168570824de04f8aea22f49f5f675d9327
davidcarbonis <alexander.david.morton@cern.ch>
2020-06-26 12:26:37 +0100

    Updated time request and email feedback

--- generation/test/ggH/batch_submit_miniAOD_ggH.sh
+++ generation/test/ggH/batch_submit_miniAOD_ggH.sh
@@ -1,23 +1,23 @@
-#qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 batch_test.sh
+#qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 batch_test.sh
 #!/bin/bash
 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS1_ctauS1/batch_miniAOD_ggH.sh 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS1_ctauS10/batch_miniAOD_ggH.sh 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS1_ctauS100/batch_miniAOD_ggH.sh 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS1_ctauS1000/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS1_ctauS1/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS1_ctauS10/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS1_ctauS100/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS1_ctauS1000/batch_miniAOD_ggH.sh 
 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS2_ctauS1/batch_miniAOD_ggH.sh 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS2_ctauS10/batch_miniAOD_ggH.sh 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS2_ctauS100/batch_miniAOD_ggH.sh 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS2_ctauS1000/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS2_ctauS1/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS2_ctauS10/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS2_ctauS100/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS2_ctauS1000/batch_miniAOD_ggH.sh 
 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS40_ctauS1/batch_miniAOD_ggH.sh 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS40_ctauS10/batch_miniAOD_ggH.sh 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS40_ctauS100/batch_miniAOD_ggH.sh 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS40_ctauS1000/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS40_ctauS1/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS40_ctauS10/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS40_ctauS100/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS40_ctauS1000/batch_miniAOD_ggH.sh 
 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS55_ctauS1/batch_miniAOD_ggH.sh 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS55_ctauS10/batch_miniAOD_ggH.sh 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS55_ctauS100/batch_miniAOD_ggH.sh 
-qsub -q hep.q -l h_rt=3600 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS55_ctauS1000/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS55_ctauS1/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS55_ctauS10/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS55_ctauS100/batch_miniAOD_ggH.sh 
+qsub -q hep.q -l h_rt=1800 -me ea -M adm10@ic.ac.uk -pe hep.pe 8 MS55_ctauS1000/batch_miniAOD_ggH.sh 
 
