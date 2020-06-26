35e0ad06523193e90725de525e1a606e91462451
davidcarbonis <alexander.david.morton@cern.ch>
2020-06-26 14:04:23 +0100

    Correct typos

--- NTupliser/test/nTuples_ggH.sh
+++ NTupliser/test/nTuples_ggH.sh
@@ -1,20 +1,20 @@
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS55_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS55_ctauS1/MC.root
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS55_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_10K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS55_ctauS10/MC.root
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS55_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS55_ctauS100/MC.root
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS55_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS55_ctauS1000/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS55_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS55_ctauS1/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS55_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_10K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS55_ctauS10/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS55_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS55_ctauS100/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS55_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS55_ctauS1000/MC.root
 
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS40_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS40_ctauS1/MC.root
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS40_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_10K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS40_ctauS10/MC.root
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS40_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS40_ctauS100/MC.root
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS40_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS40_ctauS1000/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS40_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS40_ctauS1/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS40_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_10K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS40_ctauS10/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS40_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS40_ctauS100/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS40_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS40_ctauS1000/MC.root
 
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS2_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS2_ctauS1/MC.root
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS2_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_10K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS2_ctauS10/MC.root
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS2_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS2_ctauS100/MC.root
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS2_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS2_ctauS1000/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS2_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS2_ctauS1/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS2_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_10K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS2_ctauS10/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS2_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS2_ctauS100/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS2_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS2_ctauS1000/MC.root
 
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS1_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS1_ctauS1/MC.root
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS1_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_10K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS1_ctauS10/MC.root
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS1_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS1_ctauS100/MC.root
-cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS1_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_M125_MS1_ctauS1000/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS1_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS1_ctauS1/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS1_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_10K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS1_ctauS10/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS1_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS1_ctauS100/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_HToSS_M125_MS1_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_1K.root outputFile=file:/vols/cms/adm10/nTuples_2017/ggH/NLO_HToSS_MH125_MS1_ctauS1000/MC.root
 
