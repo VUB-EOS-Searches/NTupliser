75bbcc9819bd9cf1e150a0c1690100ce28bf8299
Alexander Morton <alexander.david.morton.cern.ch>
2020-07-10 14:11:41 +0200

    Example crab generation file update

--- generation/test/Crab3Config_GEN-SIM.py
+++ generation/test/Crab3Config_GEN-SIM.py
@@ -1,25 +1,28 @@
-from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+from CRABClient.UserUtilities import config
 
 config = config()
 
-config.General.requestName = 'NLO_ggHToSSTobbbb_MH125_MS_2017_200210'
+config.General.requestName = 'NLO_HZJ_MH125_MS1_ctauS10_2017_200710'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
+config.JobType.allowUndistributedCMSSW = True
 config.JobType.pluginName = 'PrivateMC'
-config.JobType.psetName = 'NLO_ggHToSSTobbbb_MH125_MS2_2017.py'
-config.JobType.maxMemoryMB = 2500
+config.JobType.psetName = 'NLO_HZJ_HToSTodddd_MH125_Vleptonic_2017.py'
+config.JobType.maxMemoryMB = 20000
+config.JobType.numCores = 8
 
-config.Data.outputPrimaryDataset = 'NLO_ggHToSSTobbbb_MH125_MS2_2017_200310'
+config.Data.outputPrimaryDataset = 'NLO_HZJ_MH125_MS1_ctauS10_2017_200710'
 config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 10000
-NJOBS = 1  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.unitsPerJob = 50
+NJOBS = 100  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
-config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.outLFNDirBase = '/store/user/almorton/'
 
 config.Data.publication = False
-config.Data.outputDatasetTag = "CRAB3_GEN-SIM_HToSTobbbb_S2_2017_200310"
+config.Data.outputDatasetTag = "CRAB3_GEN-SIM_NLO_HZJ_MH125_MS1_ctauS10_2017_200710"
 
 config.Site.storageSite = 'T2_UK_London_IC' #T2_UK_London_IC
 #config.Site.blacklist = ['T2_UK_London_Brunel']
+
