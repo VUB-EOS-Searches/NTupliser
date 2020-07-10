665309132821ec6813e956a366093dfff299cc4d
Alexander Morton <alexander.david.morton.cern.ch>
2020-07-10 15:07:14 +0200

    comment out random number stuff as crab takes care of this and this only needs uncommenting if mass local batch jobs are required

--- generation/test/NLO_HZJ_HToSTodddd_MH125_Vleptonic_2017.py
+++ generation/test/NLO_HZJ_HToSTodddd_MH125_Vleptonic_2017.py
@@ -42,24 +42,24 @@ process.maxEvents = cms.untracked.PSet(
 # Input source
 process.source = cms.Source("EmptySource")
 
-process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
-    externalLHEProducer = cms.PSet(
-       initialSeed = cms.untracked.uint32(1),
-       engineName = cms.untracked.string('HepJamesRandom')
-    ),
-    generator = cms.PSet(
-       initialSeed = cms.untracked.uint32(2),
-       engineName = cms.untracked.string('HepJamesRandom')
-    ),
-    VtxSmeared = cms.PSet(
-       initialSeed = cms.untracked.uint32(3),
-       engineName = cms.untracked.string('HepJamesRandom')
-    ),
-    g4SimHits = cms.PSet(
-       initialSeed = cms.untracked.uint32(4),
-       engineName = cms.untracked.string('HepJamesRandom')
-    )
-)
+#process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+#    externalLHEProducer = cms.PSet(
+#       initialSeed = cms.untracked.uint32(1),
+#       engineName = cms.untracked.string('HepJamesRandom')
+#    ),
+#    generator = cms.PSet(
+#       initialSeed = cms.untracked.uint32(2),
+#       engineName = cms.untracked.string('HepJamesRandom')
+#    ),
+#    VtxSmeared = cms.PSet(
+#       initialSeed = cms.untracked.uint32(3),
+#       engineName = cms.untracked.string('HepJamesRandom')
+#    ),
+#    g4SimHits = cms.PSet(
+#       initialSeed = cms.untracked.uint32(4),
+#       engineName = cms.untracked.string('HepJamesRandom')
+#    )
+#)
 
 process.options = cms.untracked.PSet(
 )
@@ -91,13 +91,6 @@ SelectEvents = cms.untracked.PSet(
 )
 
 # Additional output definition
-
-process.RadnomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
-   externalLHEProducer = cms.PSet(
-      initialSeed = cms.untracked.uint32(2),
-      engineName = cms.untracked.string('HepJamesRandom')
-   ),
-)
 
 # Other statements
 process.XMLFromDBSource.label = cms.string("Extended")
