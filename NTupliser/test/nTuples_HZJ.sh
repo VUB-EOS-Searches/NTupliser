f999191a0d5259ff533a580b6d55d90480eba1c8
davidcarbonis <alexander.david.morton@cern.ch>
2020-07-16 21:44:09 +0100

    debug

--- NTupliser/test/nTuples_HZJ.sh
+++ NTupliser/test/nTuples_HZJ.sh
@@ -4,7 +4,7 @@ cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/HZJ/NLO_HZJ
 cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS55_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_3p5K.root outputFile=file:/vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS55_ctauS1000/MC.root
 
 cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_3p5K.root outputFile=file:/vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS1/MC.root
-msRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_3p5K.root outputFile=file:/vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS10/MC.root
+cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS10_13TeV/PAT_miniAOD/miniAOD_2017_3p5K.root outputFile=file:/vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS10/MC.root
 cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_3p5K.root outputFile=file:/vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS100/MC.root
 cmsRun nTupliserMC_miniAOD_cfg.py inputFiles=file:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS1000_13TeV/PAT_miniAOD/miniAOD_2017_3p5K.root outputFile=file:/vols/cms/adm10/nTuples_2017/HZJ/NLO_HZJ_HToSS_Vleptonic_M125_MS40_ctauS1000/MC.root
 
