72d216d2695b40485b9f8d19faf7e6ca50b6d4e7
davidcarbonis <alexander.david.morton@cern.ch>
2020-07-17 07:12:25 +0100

    Update batch script

--- NTupliser/test/batchMakeNtuplesAndSkims.sh
+++ NTupliser/test/batchMakeNtuplesAndSkims.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qsub -q hep.q -l h_rt=3600 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/NTupliser/test/makeNtuplesAndSkims.sh
+qsub -q hep.q -l h_rt=10700 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/NTupliser/test/makeNtuplesAndSkims.sh
