5f243b921cde3c5809a80736e0e867219937c305
davidcarbonis <alexander.david.morton@cern.ch>
2020-08-15 22:14:13 +0100

    Update filepaths

---
+++ generation/test/scp_ggH_part1.sh
@@ -0,0 +1,19 @@
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS55_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS55_ctauS1.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS55_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS55_ctauS10.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS55_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS55_ctauS100.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS55_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS55_ctauS1000.root
+
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS40_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS40_ctauS1.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS40_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS40_ctauS10.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS40_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS40_ctauS100.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS40_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS40_ctauS1000.root
+
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS2_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS2_ctauS1.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS2_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS2_ctauS10.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS2_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS2_ctauS100.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS2_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS2_ctauS1000.root
+
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS1_ctauS1_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS1_ctauS1.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS1_ctauS10_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS1_ctauS10.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS1_ctauS100_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS1_ctauS100.root
+scp -r adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS1_ctauS1000_13TeV/GEN-SIM/* /tmp/almorton/GEN-SIM_ggH_MS1_ctauS1000.root
---
+++ generation/test/scp_ggH_part2.sh
@@ -0,0 +1,19 @@
+scp -r /tmp/almorton/step1_ggH_MS55_ctauS1.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS55_ctauS1_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+scp -r /tmp/almorton/step1_ggH_MS55_ctauS10.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS55_ctauS10_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+scp -r /tmp/almorton/step1_ggH_MS55_ctauS100.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS55_ctauS100_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+scp -r /tmp/almorton/step1_ggH_MS55_ctauS1000.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS55_ctauS1000_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+
+scp -r /tmp/almorton/step1_ggH_MS40_ctauS1.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS40_ctauS1_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+scp -r /tmp/almorton/step1_ggH_MS40_ctauS10.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS40_ctauS10_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+scp -r /tmp/almorton/step1_ggH_MS40_ctauS100.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS40_ctauS100_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+scp -r /tmp/almorton/step1_ggH_MS40_ctauS1000.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS40_ctauS1000_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+
+scp -r  /tmp/almorton/step1_ggH_MS2_ctauS1.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS2_ctauS1_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+scp -r  /tmp/almorton/step1_ggH_MS2_ctauS10.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS2_ctauS10_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+scp -r  /tmp/almorton/step1_ggH_MS2_ctauS100.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS2_ctauS100_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+scp -r  /tmp/almorton/step1_ggH_MS2_ctauS1000.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS2_ctauS1000_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+
+scp -r  /tmp/almorton/step1_ggH_MS1_ctauS1.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS1_ctauS1_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+scp -r  /tmp/almorton/step1_ggH_MS1_ctauS10.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS1_ctauS10_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+scp -r  /tmp/almorton/step1_ggH_MS1_ctauS100.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS1_ctauS100_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
+scp -r  /tmp/almorton/step1_ggH_MS1_ctauS1000.root adm10@lx02.hep.ph.ic.ac.uk:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Shadronic_Vleptonic_M125_MS1_ctauS1000_13TeV/DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT/step1_2017_1K.root
