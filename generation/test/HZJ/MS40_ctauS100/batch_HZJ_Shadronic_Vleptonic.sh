1fad7da20cefb958b6af6af5420f145b7b925b06
davidcarbonis <alexander.david.morton@cern.ch>
2020-08-15 23:20:24 +0100

    Renamed files and update file paths

---
+++ generation/test/HZJ/MS40_ctauS100/batch_HZJ_Shadronic_Vleptonic.sh
@@ -0,0 +1,18 @@
+#qsub -q hep.q -l h_rt=35640 -pe hep.pe 8 batch_test.sh
+#!/bin/bash
+fail_exit() { echo "$@" 1>&2; exit 1; }
+
+echo "Start of job on " `date`
+
+cd /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS40_ctauS100/
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+eval `scramv1 runtime -sh`
+
+export LD_LIBRARY_PATH=`pwd`/lib/:`pwd`/lib64/:${LD_LIBRARY_PATH}
+
+cmsRun NLO_HZJ_HToSS_MH125_Shadronic_Vleptonic_2017.py outputFile=file:/vols/cms/adm10/MC/HZJ/NLO_HZJ_HToSS_Shadronic_Vleptonic_M125_MS40_ctauS100_13TeV/GEN-SIM/HZJ_MS40_ctauS100.root
+
+echo "\nEnd of job on " `date` "\n"
+
---
+++ generation/test/HZJ/batch_submit_miniAOD_HZJ_hadronic.sh
@@ -0,0 +1,23 @@
+#qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 batch_test.sh
+#!/bin/bash
+
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS1_ctauS1/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS1_ctauS10/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS1_ctauS100/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS1_ctauS1000/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS2_ctauS1/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS2_ctauS10/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS2_ctauS100/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS2_ctauS1000/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS40_ctauS1/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS40_ctauS10/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS40_ctauS100/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS40_ctauS1000/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS55_ctauS1/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS55_ctauS10/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS55_ctauS100/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+qsub -q hep.q -l h_rt=10779  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/HZJ/MS55_ctauS1000/batch_miniAOD_HZJ_Shadronic_Vleptonic.sh
+
