b132bc5bfdd1aa7fed8b542286555035d1fad064
davidcarbonis <alexander.david.morton@cern.ch>
2020-08-17 23:26:09 +0100

    Add scalar -> neutrino decay scripts

---
+++ generation/test/ggH/MS1_ctauS1000/batch_ggH_Sneutrino_Vleptonic.sh
@@ -0,0 +1,18 @@
+#qsub -q hep.q -l h_rt=35640 -pe hep.pe 8 batch_test.sh
+#!/bin/bash
+fail_exit() { echo "$@" 1>&2; exit 1; }
+
+echo "Start of job on " `date`
+
+cd /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS1000/
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+eval `scramv1 runtime -sh`
+
+export LD_LIBRARY_PATH=`pwd`/lib/:`pwd`/lib64/:${LD_LIBRARY_PATH}
+
+cmsRun NLO_ggHToSS_MH125_Sneutrino_Vleptonic_2017.py outputFile=file:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_Sneutrino_Vleptonic_M125_MS1_ctauS1000_13TeV/GEN-SIM/ggH_MS1_ctauS1000.root
+
+echo "\nEnd of job on " `date` "\n"
+
---
+++ generation/test/ggH/batch_submit_miniAOD_ggH_neutrino.sh
@@ -0,0 +1,23 @@
+#qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 batch_test.sh
+#!/bin/bash
+
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS1/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS10/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS100/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS1000/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS1/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS10/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS100/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS1000/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS40_ctauS1/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS40_ctauS10/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS40_ctauS100/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS40_ctauS1000/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS55_ctauS1/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS55_ctauS10/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS55_ctauS100/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS55_ctauS1000/batch_miniAOD_ggH_Sneutrino_Vleptonic.sh 
+
