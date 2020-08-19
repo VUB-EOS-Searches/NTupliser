73cb2e401b99fd41638bda3e3695313a99381000
davidcarbonis <alexander.david.morton@cern.ch>
2020-08-19 17:00:19 +0100

    New batch files

---
+++ generation/test/ggH/MS2_ctauS100/batch_miniAOD_ggH_SinclusiveLeptonic_Vleptonic.sh
@@ -0,0 +1,18 @@
+#qsub -q hep.q -l h_rt=35640 -pe hep.pe 8 batch_test.sh
+#!/bin/bash
+fail_exit() { echo "$@" 1>&2; exit 1; }
+
+echo "Start of job on " `date`
+
+cd /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+eval `scramv1 runtime -sh`
+
+export LD_LIBRARY_PATH=`pwd`/lib/:`pwd`/lib64/:${LD_LIBRARY_PATH}
+
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_SinclusiveLeptonic_Vleptonic_M125_MS2_ctauS100_13TeV/RAW2DIGI_RECO_EI/step2_2017_1K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_SinclusiveLeptonic_Vleptonic_M125_MS2_ctauS100_13TeV/PAT_miniAOD/miniAOD_2017_1K.root
+
+echo "\nEnd of job on " `date` "\n"
+
