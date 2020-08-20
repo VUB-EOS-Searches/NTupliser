cd2f39d2af0566fe8bc16dbee6c78ba3095920f9
davidcarbonis <alexander.david.morton@cern.ch>
2020-08-20 11:08:51 +0100

    Correct typo

--- NTupliser/test/makeNtuplesAndSkims.sh
+++ NTupliser/test/makeNtuplesAndSkims.sh
@@ -3,13 +3,13 @@ fail_exit() { echo "$@" 1>&2; exit 1; }
 
 echo "Start of job on " `date`
 
-cd /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/NTupliser/test
+cd /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/NTupliser/test
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
 eval `scramv1 runtime -sh`
 
-export LD_LIBRARY_PATH=`pwd`/lib/:`pwd`/lib64/:${LD_LIBRARY_PATH}
+#export LD_LIBRARY_PATH=`pwd`/lib/:`pwd`/lib64/:${LD_LIBRARY_PATH}
 
 bash nTuples_HZJ_Shadronic.sh 
 bash nTuples_HZJ_Sleptonic.sh 
@@ -27,7 +27,7 @@ rm /vols/cms/adm10/skims2017/ggHZ/*/*
 bash skim_HZJ_Shadronic.sh
 bash skim_HZJ_Sleptonic.sh
 
-bash skim_ggH_Shardonic.sh
+bash skim_ggH_Shadronic.sh
 bash skim_ggH_Sleptonic.sh
 
 bash skim_ggHZ_Shadronic.sh
