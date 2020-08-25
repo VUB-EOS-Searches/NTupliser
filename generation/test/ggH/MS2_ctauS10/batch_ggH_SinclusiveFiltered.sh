34d4509fbe1e50bc44e6e9d4aa7b401820ac6149
davidcarbonis <alexander.david.morton@cern.ch>
2020-08-25 21:26:01 +0100

    Update file paths

--- generation/test/ggH/MS2_ctauS10/batch_ggH_SinclusiveFiltered.sh
+++ generation/test/ggH/MS2_ctauS10/batch_ggH_SinclusiveFiltered.sh
@@ -4,7 +4,7 @@ fail_exit() { echo "$@" 1>&2; exit 1; }
 
 echo "Start of job on " `date`
 
-cd /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS10/
+cd /home/hep/adm10/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS10/
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
@@ -12,7 +12,7 @@ eval `scramv1 runtime -sh`
 
 export LD_LIBRARY_PATH=`pwd`/lib/:`pwd`/lib64/:${LD_LIBRARY_PATH}
 
-cmsRun NLO_ggHToSS_MH125_SinclusiveFiltered_Vleptonic_2017.py outputFile=file:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_SinclusiveFiltered_M125_MS2_ctauS10_13TeV/GEN-SIM/ggH_MS2_ctauS10.root
+cmsRun NLO_ggHToSS_MH125_SinclusiveFiltered_2017.py outputFile=file:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_SinclusiveFiltered_M125_MS2_ctauS10_13TeV/GEN-SIM/ggH_MS2_ctauS10.root
 
 echo "\nEnd of job on " `date` "\n"
 
