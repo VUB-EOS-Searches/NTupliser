7a205adfcfd7bb920e4b9ded4dd21e2be1b9e0a1
davidcarbonis <alexander.david.morton@cern.ch>
2020-08-25 21:08:03 +0100

    New scripts for filtered production

---
+++ generation/test/ggHZ/batch_submit_miniAOD_ggHZ_inclusiveFiltered.sh
@@ -0,0 +1,23 @@
+#qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 batch_test.sh
+#!/bin/bash
+
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS1_ctauS1/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS1_ctauS10/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS1_ctauS100/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS1_ctauS1000/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS2_ctauS1/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS2_ctauS10/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS2_ctauS100/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS2_ctauS1000/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS40_ctauS1/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS40_ctauS10/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS40_ctauS100/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS40_ctauS1000/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS55_ctauS1/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS55_ctauS10/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS55_ctauS100/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+qsub -q hep.q -l h_rt=10440  -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggHZ/MS55_ctauS1000/batch_miniAOD_ggHZ_SinclusiveFiltered_Vleptonic.sh
+
