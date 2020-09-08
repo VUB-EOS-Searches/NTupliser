6da9a7218f1dbe6f911edc7cc38284374ca79aa1
Alexander Morton <alexander.david.morton.cern.ch>
2020-09-08 19:12:07 +0200

    Updated scripts

--- generation/test/Crab3Config_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT.py
+++ generation/test/Crab3Config_DIGIPREMIX_S2_DATAMIX_L1_DIGI2RAW_HLT.py
@@ -2,7 +2,7 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 
 config = config()
 
-config.General.requestName = 'DIGI2RAW_HLT_NLO_ggH_HToSS_SmuonHadronFiltered_MH125_MS2_ctauS1000_2017_200908'
+config.General.requestName = 'DIGI2RAW_HLT_NLO_ggH_HToSS_SmuonHadronFiltered_MH125_MS1_ctauS1_2017_200908'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -22,7 +22,7 @@ config.Data.unitsPerJob = 1
 config.Data.outLFNDirBase = '/store/user/almorton/MC/DIGI2RAW_HLT/'
 
 config.Data.publication = True
-config.Data.outputDatasetTag = 'CRAB3_DIGI2RAW_HLT_NLO_ggH_HToSS_SmuonHadronFiltered_MH125_MS2_ctauS1000_2017_200908'
+config.Data.outputDatasetTag = 'CRAB3_DIGI2RAW_HLT_NLO_ggH_HToSS_SmuonHadronFiltered_MH125_MS1_ctauS1_2017_200908'
 
 config.Site.storageSite = 'T2_BE_IIHE' #T2_UK_London_IC, T2_UK_London_Brunel, T2_BE_IIHE
 #config.Site.blacklist = ['T2_UK_London_Brunel']
