0267872b2503eed588806cf922c3dc09f874d314
davidcarbonis <alexander.david.morton@cern.ch>
2020-09-09 16:34:15 +0100

    Update scripts

--- generation/test/ggH/MS40_ctauS1/batch_miniAOD_ggH_SinclusiveFiltered.sh
+++ generation/test/ggH/MS40_ctauS1/batch_miniAOD_ggH_SinclusiveFiltered.sh
@@ -12,7 +12,7 @@ eval `scramv1 runtime -sh`
 
 export LD_LIBRARY_PATH=`pwd`/lib/:`pwd`/lib64/:${LD_LIBRARY_PATH}
 
-cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_SinclusiveFiltered_M125_MS40_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_1p4K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_SinclusiveFiltered_M125_MS40_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_1p4K.root
+cmsRun step3_PAT_miniAOD_2017.py inputFiles=file:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_SinclusiveFiltered_M125_MS40_ctauS1_13TeV/RAW2DIGI_RECO_EI/step2_2017_3p5K.root outputFile=file:/vols/cms/adm10/MC/ggH/NLO_ggH_HToSS_SinclusiveFiltered_M125_MS40_ctauS1_13TeV/PAT_miniAOD/miniAOD_2017_3p5K.root
 
 echo "\nEnd of job on " `date` "\n"
 
