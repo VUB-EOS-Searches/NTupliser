e065afb95156646d689e32ab14f4d2a8a24c93f5
davidcarbonis <alexander.david.morton@cern.ch>
2020-09-14 09:07:30 +0100

    Update batch script

--- generation/test/ggH/batch_submit_miniAOD_ggH_inclusiveFiltered.sh
+++ generation/test/ggH/batch_submit_miniAOD_ggH_inclusiveFiltered.sh
@@ -1,23 +1,23 @@
-#qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 batch_test.sh
+#qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 batch_test.sh
 #!/bin/bash
 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS1/batch_miniAOD_ggH_SinclusiveFiltered.sh 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS10/batch_miniAOD_ggH_SinclusiveFiltered.sh 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS100/batch_miniAOD_ggH_SinclusiveFiltered.sh 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS1000/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS1/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS10/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS100/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS1_ctauS1000/batch_miniAOD_ggH_SinclusiveFiltered.sh 
 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS1/batch_miniAOD_ggH_SinclusiveFiltered.sh 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS10/batch_miniAOD_ggH_SinclusiveFiltered.sh 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS100/batch_miniAOD_ggH_SinclusiveFiltered.sh 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS1000/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS1/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS10/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS100/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS2_ctauS1000/batch_miniAOD_ggH_SinclusiveFiltered.sh 
 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS40_ctauS1/batch_miniAOD_ggH_SinclusiveFiltered.sh 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS40_ctauS10/batch_miniAOD_ggH_SinclusiveFiltered.sh 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS40_ctauS100/batch_miniAOD_ggH_SinclusiveFiltered.sh 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS40_ctauS1000/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS40_ctauS1/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS40_ctauS10/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS40_ctauS100/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS40_ctauS1000/batch_miniAOD_ggH_SinclusiveFiltered.sh 
 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS55_ctauS1/batch_miniAOD_ggH_SinclusiveFiltered.sh 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS55_ctauS10/batch_miniAOD_ggH_SinclusiveFiltered.sh 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS55_ctauS100/batch_miniAOD_ggH_SinclusiveFiltered.sh 
-qsub -q hep.q -l h_rt=1800 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS55_ctauS1000/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS55_ctauS1/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS55_ctauS10/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS55_ctauS100/batch_miniAOD_ggH_SinclusiveFiltered.sh 
+qsub -q hep.q -l h_rt=10799 -m ea -M adm10@ic.ac.uk -pe hep.pe 8 /home/hep/adm10/CMSSW/CMSSW_9_4_8/src/NTupliser/generation/test/ggH/MS55_ctauS1000/batch_miniAOD_ggH_SinclusiveFiltered.sh 
 
