3d530bd3d9765a4dc7c13d9d811e03ce4adca37c
davidcarbonis <alexander.david.morton@cern.ch>
2020-12-26 22:40:26 +0100

    WIP

--- generation/test/NLO_ggHToSSTodddd_MH125_MSAAAA_ctauSBBBB_2018.py
+++ generation/test/NLO_ggHToSSTodddd_MH125_MSAAAA_ctauSBBBB_2018.py
@@ -25,19 +25,33 @@ process.load('Configuration.StandardSequences.SimIdeal_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
+intEvts  =  cms.untracked.int32(10000)
+uintEvts = cms.untracked.uint32(10000)
+
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1)
+    input = intEvts
 )
 
 # Input source
-process.source = cms.Source("PoolSource",
-    dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
-    fileNames = cms.untracked.vstring('file:step-1.root'),
-    inputCommands = cms.untracked.vstring(
-        'keep *', 
-        'drop LHEXMLStringProduct_*_*_*'
+process.source = cms.Source("EmptySource")
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    externalLHEProducer = cms.PSet(
+       initialSeed = cms.untracked.uint32(1),
+       engineName = cms.untracked.string('HepJamesRandom')
     ),
-    secondaryFileNames = cms.untracked.vstring()
+    generator = cms.PSet(
+       initialSeed = cms.untracked.uint32(2),
+       engineName = cms.untracked.string('HepJamesRandom')
+    ),
+    VtxSmeared = cms.PSet(
+       initialSeed = cms.untracked.uint32(3),
+       engineName = cms.untracked.string('HepJamesRandom')
+    ),
+    g4SimHits = cms.PSet(
+       initialSeed = cms.untracked.uint32(4),
+       engineName = cms.untracked.string('HepJamesRandom')
+    )
 )
 
 process.options = cms.untracked.PSet(
@@ -77,12 +91,92 @@ process.genstepfilter.triggerConditions=cms.vstring("generation_step")
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, '102X_upgrade2018_realistic_v9', '')
 
+process.generator = cms.EDFilter("Pythia8HadronizerFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring('pythia8CommonSettings',
+            'pythia8CP2Settings',
+            'pythia8PowhegEmissionVetoSettings',
+            'processParameters'),
+        processParameters = cms.vstring('POWHEG:nFinal = 1',
+#            <particle id="..." name, antiName, spinType=, chargeType, colType, m0, mWidth, mMin, mMax, tau0> ##tau0 = nominal proper lifetime (mm/c)
+#            '9000006:all = sk   skbar    0        0          0       2.0  0.0197327e-11/tau0  1.0  75.0 tau0',
+            '9000006:all = sk   skbar    0        0          0       1.0  1.9732e-14  1.0  75.0 10',
+            '9000006:oneChannel = 1  1.0 101  1 -1',
+            '9000006:addChannel = 1  1.0 101  2 -2',
+            '9000006:addChannel = 1  1.0 101  3 -3',
+            '9000006:addChannel = 1  1.0 101  4 -4',
+            '9000006:addChannel = 1  1.0 101  5 -5',
+            '9000006:mayDecay = on',
+            '9000006:isResonance = on',
+            '25:m0 = 125.0',
+            '25:onMode = off',
+            '25:addChannel = 1 0.000000001 101 9000006 -9000006',
+            '25:onIfMatch = 9000006 -9000006',
+            '9000006:onMode = off',
+            '9000006:onIfAny = 1',
+            '9000006:onIfAny = 2',
+            '9000006:onIfAny = 3',
+            '9000006:onIfAny = 4',
+            '9000006:onIfAny = 5'),
+        pythia8CP2Settings = cms.vstring('Tune:pp 14',
+            'Tune:ee 7',
+            'MultipartonInteractions:ecmPow=0.1391',
+            'PDF:pSet=17',
+            'MultipartonInteractions:bProfile=2',
+            'MultipartonInteractions:pT0Ref=2.306',
+            'MultipartonInteractions:coreRadius=0.3755',
+            'MultipartonInteractions:coreFraction=0.3269',
+            'ColourReconnection:range=2.323',
+            'SigmaTotal:zeroAXB=off',
+            'SpaceShower:rapidityOrder=off',
+            'SpaceShower:alphaSvalue=0.13',
+            'TimeShower:alphaSvalue=0.13'),
+        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2',
+            'Main:timesAllowErrors = 10000',
+            'Check:epTolErr = 0.01',
+            'Beams:setProductionScalesFromLHEF = off',
+            'SLHA:keepSM = on',
+            'SLHA:minMassSM = 1000.',
+            'ParticleDecays:limitTau0 = on',
+            'ParticleDecays:tau0Max = 10',
+            'ParticleDecays:allowPhotonRadiation = on'),
+        pythia8PowhegEmissionVetoSettings = cms.vstring('POWHEG:veto = 1',
+            'POWHEG:pTdef = 1',
+            'POWHEG:emitted = 0',
+            'POWHEG:pTemt = 0',
+            'POWHEG:pThard = 0',
+            'POWHEG:vetoCount = 100',
+            'SpaceShower:pTmaxMatch = 2',
+            'TimeShower:pTmaxMatch = 2')
+    ),
+    comEnergy = cms.double(13000.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(1)
+)
+
+process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/2017/13TeV/powheg/V2/gg_H_quark-mass-effects_NNPDF31_13TeV_M125/v1/gg_H_quark-mass-effects_NNPDF31_13TeV_M125_slc6_amd64_gcc630_CMSSW_9$
+    nEvents = uintEvts,
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+process.ProductionFilterSequence = cms.Sequence(process.generator)
+
 # Path and EndPath definitions
+process.lhe_step = cms.Path(process.externalLHEProducer)
 process.generation_step = cms.Path(process.pgen)
 process.simulation_step = cms.Path(process.psim)
 process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
 process.endjob_step = cms.EndPath(process.endOfProcess)
 process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+#Setup FWK for multithreaded
+#process.options.numberOfThreads=cms.untracked.uint32(8)
+#process.options.numberOfStreams=cms.untracked.uint32(8)
 
 # Schedule definition
 process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
@@ -94,9 +188,20 @@ process.options.numberOfThreads=cms.untracked.uint32(8)
 process.options.numberOfStreams=cms.untracked.uint32(0)
 
 
+# Schedule definition
+process.schedule = cms.Schedule(process.lhe_step,process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+# filter all path with the production filter sequence
+for path in process.paths:
+        if path in ['lhe_step']: continue
+        getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq
+
+
 # Customisation from command line
 
 # Add early deletion of temporary data products to reduce peak memory need
 from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
 process = customiseEarlyDelete(process)
 # End adding early deletion
+
